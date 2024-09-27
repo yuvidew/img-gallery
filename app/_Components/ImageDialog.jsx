e186334a0fb294ce43406cb04e0297fd85d98137
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2024-09-27 13:43:49 +0530

    update the file

--- app/_Components/ImageDialog.jsx
+++ app/_Components/ImageDialog.jsx
@@ -1,4 +1,3 @@
-import React ,{ useMemo, useState } from 'react'
 import {
     Dialog,
     DialogContent,
@@ -7,6 +6,7 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import Image from 'next/image'
 
 
 export const ImageDialog = ({
@@ -17,13 +17,27 @@ export const ImageDialog = ({
     return (
         <Dialog>
         <DialogTrigger>
-            <img className = "w-full mb-2" src = {src} alt = {alt} />
+            <Image
+                className="w-full mb-2"  // Tailwind CSS classes
+                src={src}                // Image source
+                alt={alt}                // Alt text
+                layout="responsive"      // Responsive layout
+                width={16}               // Aspect ratio: width
+                height={9}  
+            />
         </DialogTrigger>
         <DialogContent className = "  ">
             <DialogHeader className={"h-full "}>
             <DialogTitle>{alt}</DialogTitle>
                 <DialogDescription className = "h-full flex items-center justify-center">
-                    <img className = " w-full mb-2 " src = {src} alt = {alt} />
+                    <Image 
+                        className="w-full mb-2"  // Tailwind CSS classes
+                        src={src}                // Image source
+                        alt={alt}                // Alt text
+                        layout="responsive"      // Responsive layout
+                        width={16}               // Aspect ratio: width
+                        height={9}  
+                    />
                 </DialogDescription>
             </DialogHeader>
         </DialogContent>
